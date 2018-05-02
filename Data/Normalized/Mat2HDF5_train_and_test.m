d3629eec585f81b99e0406e6d88c8ab2992ba22f
trakaros <caxelos@inf.uth.gr>
2018-05-02 17:13:50 +0300

    train and test creation simultaneously

--- Data/Normalized/Mat2HDF5_train_and_test.m
+++ Data/Normalized/Mat2HDF5_train_and_test.m
@@ -3,45 +3,22 @@ clear;
 clc;
 
 
-
-%Pij lists all p00, p01, p02,...
-dirData = dir(pwd);
-dirIndex = [dirData.isdir];
-Pij = dirData(dirIndex);
-
-%for each Pij...
-for num_Pij=3:length(Pij)
-filepath = strcat(Pij(num_Pij).name, '/'); %'p00/';%'MPIIGaze/';
- 
-dirData = dir(filepath);%path = dir(filepath);
-dirIndex = [dirData.isdir];
-files = {dirData(~dirIndex).name}';
-%path = path(3:end);
-%files = {path.name};
-
-%total_num=0;
-%for num_f=1:length(files)
-%    readname = [filepath, files{num_f}];
-%    temp = load(readname);  
-%    total_num = total_num+length(temp.filenames(1,:));%(temp.errors);
-%end
-
 fprintf('File Path Ready!\n');
 
 %training
 trainData=[];
-trainData.data = zeros(60,36,1, 1125*2);%zeros(60,36,1, total_num*2);
-trainData.label = zeros(2, 1125*2);%zeros(2, total_num*2);
-trainData.headpose = zeros(2, 1125*2);%zeros(2, total_num*2);
-trainData.confidence = zeros(1, 1125*2);%zeros(1, total_num*2);
+trainData.data = zeros(60,36,1, 15*1125*2);%zeros(60,36,1, total_num*2);
+trainData.label = zeros(2, 15*1125*2);%zeros(2, total_num*2);
+trainData.headpose = zeros(2, 15*1125*2);%zeros(2, total_num*2);
+trainData.confidence = zeros(1, 15*1125*2);%zeros(1, total_num*2);
 trainindex = 0;
 
 %test
 testData=[];
-testData.data = zeros(60,36,1, 375*2);%zeros(60,36,1, total_num*2);
-testData.label = zeros(2, 375*2);%zeros(2, total_num*2);
-testData.headpose = zeros(2, 375*2);%zeros(2, total_num*2);
-testData.confidence = zeros(1, 375*2);%zeros(1, total_num*2);
+testData.data = zeros(60,36,1, 15*375*2);%zeros(60,36,1, total_num*2);
+testData.label = zeros(2, 15*375*2);%zeros(2, total_num*2);
+testData.headpose = zeros(2, 15*375*2);%zeros(2, total_num*2);
+testData.confidence = zeros(1, 15*375*2);%zeros(1, total_num*2);
 testindex = 0;
 
 %temp
@@ -50,22 +27,39 @@ tempData.data = zeros(60,36,1, 1*2);%zeros(60,36,1, total_num*2);
 tempData.label = zeros(2, 1*2);%zeros(2, total_num*2);
 tempData.headpose = zeros(2, 1*2);%zeros(2, total_num*2);
 tempData.confidence = zeros(1, 1*2);%zeros(1, total_num*2);
-tempindex = 0;
 
 
+%Pij lists all p00, p01, p02,...
+dirData = dir(pwd);
+dirIndex = [dirData.isdir];
+Pij = dirData(dirIndex);
+%for each Pij...
+for num_Pij=3:length(Pij)
+  filepath = strcat(Pij(num_Pij).name, '/'); %'p00/';%'MPIIGaze/';
  
-step_size = get_step_size(1500, filepath);
-ratio = 3; % 75% are for training, 25% for test
-curr_sample = 0;
+  %%% LIST ALL FILES %%%
+  dirData = dir(filepath);%path = dir(filepath);
+  dirIndex = [dirData.isdir];
+  files = {dirData(~dirIndex).name}';
 
-for num_f=1:length(files) 
+  %%%% STEPS %%%%
+  step_size = get_step_size(1500, filepath);
+  curr_step = 1;
 
+  %%% TRAINING vs TEST RATIO(75%) %%%
+  ratio = 3; % 75% are for training, 25% for test
+  curr_ratio = 0;
 
+  for num_f=1:length(files) 
+   
     readname = [filepath, files{num_f}];
     temp = load(readname);   
     num_data = length(temp.filenames(:,1));   
     for num_i=1:num_data
-	
+      if curr_step == step_size 	
+	curr_step = 1;
+
+
         % for left
         img = temp.data.left.image(num_i, :,:);
         img = reshape(img, 36,60);
@@ -101,39 +95,45 @@ for num_f=1:length(files)
         tempData.headpose(:,2) = [theta; (-1)*phi]; % flip the direction
 
 
-	if mod(curr_sample, ratio) == 0
+	if  curr_ratio == 3 %0
+		curr_ratio = 0;
 		%%%%%%%%%%%%%%%
 		% TEST DATA
 		%%%%%%%%%%%%%%%
 		%copy left
 		testindex = testindex+1;
-		testData.data(:, :, 1, testindex)
-		testData.label(:,testindex)
-		testData.headpose(:,testindex)
+		testData.data(:, :, 1, testindex) = tempData.data(:, :, 1,1);
+		testData.label(:,testindex) = tempData.label(:,1);
+		testData.headpose(:,testindex) = tempData.headpose(:,1);
 
 		%copy right
 		testindex = testindex+1;
-		testData.data(:, :, 1, testindex);
-		testData.label(:,testindex);
-		testData.headpose(:,testindex)
-	else
+		testData.data(:, :, 1, testindex) = tempData.data(:, :, 1, 2);
+		testData.label(:,testindex) = tempData.label(:,2);
+		testData.headpose(:,testindex) = tempData.headpose(:,2);
+	else %0,1,2
+		curr_ratio = curr_ratio + 1;
 		%%%%%%%%%%%%%%%
                 % TRAINING DATA
                 %%%%%%%%%%%%%%%
 
 		%copy left
                 trainindex = trainindex+1;
-                trainData.data(:, :, 1, trainindex)
-                trainData.label(:,trainindex)
-                trainData.headpose(:,trainindex)
+                trainData.data(:, :, 1, trainindex) = tempData.data(:, :, 1,1);
+                trainData.label(:,trainindex) = tempData.label(:,1);
+                trainData.headpose(:,trainindex) = tempData.headpose(:,1);
 
                 %copy right
                 trainindex = trainindex+1;
-                trainData.data(:, :, 1, trainindex);
-                trainData.label(:,trainindex);
-                trainData.headpose(:,trainindex)
+                trainData.data(:, :, 1, trainindex) = tempData.data(:, :, 1, 2);
+                trainData.label(:,trainindex) =  tempData.label(:,2);
+                trainData.headpose(:,trainindex) = tempData.headpose(:,2);
 
-	end	
+	end
+
+     else % not in the samples
+	curr_step = curr_step + 1;
+     end	
     end
 
     fprintf('%d / %d !\n', num_f, length(files)); 
@@ -156,7 +156,7 @@ trainData.headpose = single(trainData.headpose);
 savename = 'MPII_traindata.h5';
 %store2hdf5(savename, Data.data, Data.label, 1, 1); % the store2hdf5 function comes from https://github.com/BVLC/caffe/pull/1746
 %% You can also use the matlab function for hdf5 saving:
- hdf5write(savename,'/data', trainData.data, '/label',[trainData.label;
+hdf5write(savename,'/data', trainData.data, '/label',[trainData.label;
 trainData.headpose]); 
 fprintf('done\n');
 
@@ -167,8 +167,6 @@ savename = 'MPII_testdata.h5';
  hdf5write(savename,'/data', testData.data, '/label',[testData.label;
 testData.headpose]); 
 fprintf('done\n');
-
-
 
 
 
