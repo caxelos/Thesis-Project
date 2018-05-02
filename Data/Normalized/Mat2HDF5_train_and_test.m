edcbc8f3d1a1cf333a266447cb8c315942c8ce57
trakaros <caxelos@inf.uth.gr>
2018-05-02 15:05:50 +0300

    train and test creation simultaneously

--- Data/Normalized/Mat2HDF5_train_and_test.m
+++ Data/Normalized/Mat2HDF5_train_and_test.m
@@ -29,12 +29,12 @@ files = {dirData(~dirIndex).name}';
 fprintf('File Path Ready!\n');
 
 %training
-trainingData=[];
-trainingData.data = zeros(60,36,1, 1125*2);%zeros(60,36,1, total_num*2);
-trainingData.label = zeros(2, 1125*2);%zeros(2, total_num*2);
-trainingData.headpose = zeros(2, 1125*2);%zeros(2, total_num*2);
-trainingData.confidence = zeros(1, 1125*2);%zeros(1, total_num*2);
-trainingindex = 0;
+trainData=[];
+trainData.data = zeros(60,36,1, 1125*2);%zeros(60,36,1, total_num*2);
+trainData.label = zeros(2, 1125*2);%zeros(2, total_num*2);
+trainData.headpose = zeros(2, 1125*2);%zeros(2, total_num*2);
+trainData.confidence = zeros(1, 1125*2);%zeros(1, total_num*2);
+trainindex = 0;
 
 %test
 testData=[];
@@ -67,40 +67,38 @@ for num_f=1:length(files)
     for num_i=1:num_data
 	
         % for left
-        index = index+1;
         img = temp.data.left.image(num_i, :,:);
         img = reshape(img, 36,60);
-        Data.data(:, :, 1, index) = img'; % filp the image
+        tempData.data(:, :, 1, 1) = img'; % filp the image
         
         Lable_left = temp.data.left.gaze(num_i, :)';
         theta = asin((-1)*Lable_left(2));
         phi = atan2((-1)*Lable_left(1), (-1)*Lable_left(3));
-        Data.label(:,index) = [theta; phi];
+        tempData.label(:,1) = [theta; phi];
  
         headpose = temp.data.left.pose(num_i, :);
         M = rodrigues(headpose);
         Zv = M(:,3);
         theta = asin(Zv(2));
         phi = atan2(Zv(1), Zv(3));
-        Data.headpose(:,index) = [theta;phi];         
+        tempData.headpose(:,1) = [theta;phi];         
          
         % for right
-        index = index+1;
         img = temp.data.right.image(num_i, :,:);
         img = reshape(img, 36,60);
-        Data.data(:, :, 1, index) = double(flip(img, 2))'; % filp the image
+        tempData.data(:, :, 1, 2) = double(flip(img, 2))'; % filp the image
          
         Lable_right = temp.data.right.gaze(num_i,:)';
         theta = asin((-1)*Lable_right(2));
         phi = atan2((-1)*Lable_right(1), (-1)*Lable_right(3));
-        Data.label(:,index) = [theta; (-1)*phi];% flip the direction
+        tempData.label(:,2) = [theta; (-1)*phi];% flip the direction
 
         headpose = temp.data.right.pose(num_i, :); 
         M = rodrigues(headpose);
         Zv = M(:,3);
         theta = asin(Zv(2));
        	phi = atan2(Zv(1), Zv(3));
-        Data.headpose(:,index) = [theta; (-1)*phi]; % flip the direction
+        tempData.headpose(:,2) = [theta; (-1)*phi]; % flip the direction
 
 
 	if mod(curr_sample, ratio) == 0
@@ -109,34 +107,31 @@ for num_f=1:length(files)
 		%%%%%%%%%%%%%%%
 		%copy left
 		testindex = testindex+1;
-		testData.data(:, :, 1, trainingindex)
-		testData.label(:,index)
-		testData.headpose(:,index)
+		testData.data(:, :, 1, testindex)
+		testData.label(:,testindex)
+		testData.headpose(:,testindex)
 
 		%copy right
 		testindex = testindex+1;
-		testData.data(:, :, 1, index);
-		testData.label(:,index);
-		testData.headpose(:,index)
+		testData.data(:, :, 1, testindex);
+		testData.label(:,testindex);
+		testData.headpose(:,testindex)
 	else
 		%%%%%%%%%%%%%%%
                 % TRAINING DATA
                 %%%%%%%%%%%%%%%
 
-
 		%copy left
-                trainingindex = trainingindex+1;
-                trainingData.data(:, :, 1, trainingindex)
-                trainingData.label(:,index)
-                Data.headpose(:,index)
+                trainindex = trainindex+1;
+                trainData.data(:, :, 1, trainindex)
+                trainData.label(:,trainindex)
+                trainData.headpose(:,trainindex)
 
                 %copy right
-                trainingindex = trainingindex+1;
-                Data.data(:, :, 1, index);
-                Data.label(:,index);
-                Data.headpose(:,index)
-
-
+                trainindex = trainindex+1;
+                trainData.data(:, :, 1, trainindex);
+                trainData.label(:,trainindex);
+                trainData.headpose(:,trainindex)
 
 	end	
     end
@@ -146,17 +141,35 @@ end
  
 fprintf('Saving\n');
 
-Data.data = Data.data/255; %normalize
-Data.data = single(Data.data); % must be single data, because caffe want float type
-Data.label = single(Data.label);
-Data.headpose = single(Data.headpose);
+testData.data = testData.data/255; %normalize
+testData.data = single(testData.data); % must be single data, because caffe want float type
+testData.label = single(testData.label);
+testData.headpose = single(testData.headpose);
 
-savename = 'MPII_data.h5';
+trainData.data = trainData.data/255; %normalize
+trainData.data = single(trainData.data); % must be single data, because caffe want
+trainData.label = single(trainData.label);
+trainData.headpose = single(trainData.headpose);
 
+
+
+savename = 'MPII_traindata.h5';
 %store2hdf5(savename, Data.data, Data.label, 1, 1); % the store2hdf5 function comes from https://github.com/BVLC/caffe/pull/1746
 %% You can also use the matlab function for hdf5 saving:
- hdf5write(savename,'/data', Data.data, '/label',[Data.label; Data.headpose]); 
+ hdf5write(savename,'/data', trainData.data, '/label',[trainData.label;
+trainData.headpose]); 
 fprintf('done\n');
+
+
+savename = 'MPII_testdata.h5';
+%store2hdf5(savename, Data.data, Data.label, 1, 1); % the store2hdf5 function co
+%% You can also use the matlab function for hdf5 saving:
+ hdf5write(savename,'/data', testData.data, '/label',[testData.label;
+testData.headpose]); 
+fprintf('done\n');
+
+
+
 
 
 end
