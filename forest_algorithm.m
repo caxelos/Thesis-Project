6039f16e1e74d849b96ddc691ba636f495c3c72c
trakaros <caxelos@inf.uth.gr>
2018-06-05 18:27:23 +0300

    forest gump run!

--- forest_algorithm.m
+++ forest_algorithm.m
@@ -1,3 +1,5 @@
+function forest_algorithm()
+
 clear all;
 clc;
 
@@ -18,19 +20,50 @@ grpID = H5G.open(fid, '/g1');
 
 
 %%%%%%%%%% Open Specific dataset of a group %
-dsetID = H5D.open(grpID, '5_nearestIDs');
+rnearestID      = H5D.open(grpID, '5_nearestIDs');
+centerID        = H5D.open(grpID, 'center');
+dataID          = H5D.open(grpID, 'data');
+labelID		= H5D.open(grpID, 'label');
+
+
 
 
 
 %%%%%%%%% Read Data %%%%%%%%%%%%%%%%%%%%%%%%%
-data = H5D.read(dsetID)
+rnearest = H5D.read(rnearestID)
 
 
-H5D.close(dsetID);
+
+%%%%%%%% Now Start building tree(i) %%%%%%%%%
+%i need: images, headposes, gazes -> data, label
+
+for i = 1:R 
+	localGrpIDs(i) = H5G.open(fid, strcat('/g', num2str( rnearest(i))   )); 
+	tempDataID(i) =  H5D.open( localGrpIDs(i),  strcat('/g', num2str( rnearest(i) ), '/label') );
+
+	
+	tempData = H5D.read(tempDataID);
+end
+tempData
+
+
+%%% doesnt run here
+for i = 1:R
+	H5D.close( tempDataID(i) );
+	H5G.close( localGrpIDs(i) ) ;
+end
+%%%%%%%%% Close Everything %%%%%%%%%%%%%%%%%%
+H5D.close(rnearestID);
+H5D.close(centerID);
+H5D.close(dataID);
+H5D.close(labelID);
+
+
+
 H5G.close(grpID);
 H5F.close(fid);
 
 
-
+end
 
 %H5G.get_info(grpID)
