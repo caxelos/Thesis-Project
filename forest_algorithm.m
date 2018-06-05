005f7dcc4ebeeb997fd3ce198d1732a7c69af0f6
trakaros <caxelos@inf.uth.gr>
2018-06-06 00:35:29 +0300

    forest gump run!

--- forest_algorithm.m
+++ forest_algorithm.m
@@ -13,104 +13,107 @@ fid = H5F.open('myfile.h5', 'H5F_ACC_RDONLY', 'H5P_DEFAULT');
 
 
 
-samplesInTree = 0;
-%%%%%%%%%% Start with the central group %%%%%%%%%%
-grpID = H5G.open(fid, '/g1');
-
-curr_rnearestID      = H5D.open(grpID, '5_nearestIDs');
-curr_centerID        = H5D.open(grpID, 'center');
-curr_imgsID          = H5D.open(grpID, 'data');
-curr_gazesID 	= H5D.open(grpID, 'gaze');
-curr_posesID		= H5D.open(grpID, 'headpose');
-
-curr_rnearest = H5D.read(curr_rnearestID);
-curr_center   = H5D.read(curr_centerID);
-curr_imgs     = H5D.read(curr_imgsID);
-curr_gazes    = H5D.read(curr_gazesID);
-curr_poses    = H5D.read(curr_posesID);
-
-samplesInGroup = length( curr_imgs(:,1,1,1) );
-contribOfGroup = ceil( sqrt( samplesInGroup ) );
-
-j = 1;
-while j <= contribOfGroup
-
-	samplesInTree = samplesInTree + 1;
-	random = randi(samplesInGroup,1,1);
-	treeImgs(samplesInTree, :, :) =  curr_imgs( random  ,1, :, :);
-	treeGazes(samplesInTree, :) = curr_gazes( random, :);%, :);
-	treePoses(samplesInTree, :) = curr_poses( random, :);
-		
-	j = j + 1;		
-
-end
 
 
-
-%%%%%%%% Now, continue with the R-nearest %%%%%%%%%
-
+for i = 1:140 %for every tree
 
 
+	samplesInTree(i) = 0;
+	%%%%%%%%%% Start with the central group %%%%%%%%%%
+	grpID = H5G.open(fid, strcat('/g',num2str(i)) );
 
-for i = 1:R 
-	localGrpID  = H5G.open(fid, strcat('/g', num2str( curr_rnearest(i))   )); 
+	curr_rnearestID      = H5D.open(grpID, '5_nearestIDs');
+	curr_centerID        = H5D.open(grpID, 'center');
+	curr_imgsID          = H5D.open(grpID, 'data');
+	curr_gazesID 	= H5D.open(grpID, 'gaze');
+	curr_posesID		= H5D.open(grpID, 'headpose');
 
-	tempImgID  = H5D.open( localGrpID,  strcat('/g', num2str( curr_rnearest(i) ), '/data') );
-	tempPoseID = H5D.open( localGrpID,  strcat('/g', num2str( curr_rnearest(i) ), '/headpose') );
-	tempGazeID = H5D.open( localGrpID,  strcat('/g', num2str( curr_rnearest(i) ), '/gaze') );
-	
-	tempImgs = H5D.read( tempImgID );
-	tempPoses = H5D.read( tempPoseID );
-	tempGazes = H5D.read( tempGazeID );
+	curr_rnearest = H5D.read(curr_rnearestID);
+	curr_center   = H5D.read(curr_centerID);
+	curr_imgs     = H5D.read(curr_imgsID);
+	curr_gazes    = H5D.read(curr_gazesID);
+	curr_poses    = H5D.read(curr_posesID);
 
-
-
-	samplesInGroup = length( tempImgs(:,1,1,1) );
+	samplesInGroup = length( curr_imgs(:,1,1,1) );
 	contribOfGroup = ceil( sqrt( samplesInGroup ) );
+
 	j = 1;
 	while j <= contribOfGroup
 
-		samplesInTree = samplesInTree + 1;
+		samplesInTree(i) = samplesInTree(i) + 1;
 		random = randi(samplesInGroup,1,1);
-		treeImgs(samplesInTree, :, :) =  tempImgs( random  ,1, :, :);
-		treeGazes(samplesInTree, :) = tempGazes( random, :);%, :);
-		treePoses(samplesInTree, :) = tempPoses( random, :);
+		treeImgs (i, samplesInTree(i), :, :) =  curr_imgs( random  ,1, :, :);
+		treeGazes(i, samplesInTree(i), :) = curr_gazes( random, :);%, :);
+		treePoses(i, samplesInTree(i), :) = curr_poses( random, :);
 		
 		j = j + 1;		
 
 	end
 
-	H5D.close( tempImgID );
-	H5D.close( tempPoseID);
-	H5D.close( tempGazeID);
-
-	H5G.close( localGrpID ) ;
-
-end
 
 
-samplesInTree
-for i = 1:samplesInTree
-	%treeLabel(i, 4)
-end
+	%%%%%%%% Now, continue with the R-nearest %%%%%%%%%
 
 
 
+	
+	for k = 1:R 
+			
+		localGrpID  = H5G.open(fid, strcat('/g', num2str( curr_rnearest(k))   )); 
 
-%%%%%%%%% Close Everything %%%%%%%%%%%%%%%%%%
+		tempImgID  = H5D.open( localGrpID,  strcat('/g', num2str( curr_rnearest(k) ), '/data') );
+		tempPoseID = H5D.open( localGrpID,  strcat('/g', num2str( curr_rnearest(k) ), '/headpose') );
+		tempGazeID = H5D.open( localGrpID,  strcat('/g', num2str( curr_rnearest(k) ), '/gaze') );
+	
+		tempImgs = H5D.read( tempImgID );
+		tempPoses = H5D.read( tempPoseID );
+		tempGazes = H5D.read( tempGazeID );
 
-H5D.close(curr_rnearestID);
-H5D.close(curr_centerID);
-H5D.close(curr_imgsID);
-H5D.close(curr_gazesID);
-H5D.close(curr_posesID);
+		samplesInGroup = length( tempImgs(:,1,1,1) );
+		contribOfGroup = ceil( sqrt( samplesInGroup ) );
+		j = 1;
+		while j <= contribOfGroup
+
+			samplesInTree(i) = samplesInTree(i) + 1;
+			random = randi(samplesInGroup,1,1);
+			treeImgs (i, samplesInTree(i), :, :) =  tempImgs( random  ,1, :, :);
+			treeGazes(i, samplesInTree(i), :) = tempGazes( random, :);%, :);
+			treePoses(i, samplesInTree(i), :) = tempPoses( random, :);
+		
+			j = j + 1;		
+
+		end
+
+		H5D.close( tempImgID );
+		H5D.close( tempPoseID);
+		H5D.close( tempGazeID);
+
+		H5G.close( localGrpID ) ;
+
+	end
+
+	%%%%%%%%% Close Central Group %%%%%%%%%%%%%%%%%%
+	H5D.close(curr_rnearestID);
+	H5D.close(curr_centerID);
+	H5D.close(curr_imgsID);
+	H5D.close(curr_gazesID);
+	H5D.close(curr_posesID);
+
+	H5G.close(grpID);
 
 
+	
 
-H5G.close(grpID);
+
+end	
+
+
+treePoses(120, 1:(samplesInTree(120)+1), 1)
+
+
 H5F.close(fid);
 
 
 end
 
-%H5G.get_info(grpID)
+%H5G.get_info(grpID) 
