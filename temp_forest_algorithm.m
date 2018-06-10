0d36de19f65da61a20f437d9300a1c177f837586
trakaros <caxelos@inf.uth.gr>
2018-06-10 15:07:45 +0300

    run forest run

--- temp_forest_algorithm.m
+++ temp_forest_algorithm.m
@@ -1,4 +1,4 @@
-function tempforest
+function forest_algorithm()
 
 clear all;
 clc;
@@ -131,7 +131,7 @@ end
 	   if i == 128 || i == 32 || i == 129 || i == 91 || i == 130 || i == 126
 	      
 	      trees(i) = tree(strcat('RegressionTree_', num2str(i) ));
-	      trees(i) = buildRegressionTree( samplesInTree(i), treeImgs(i,:,:,:),  treeGazes(i,:,:), HEIGHT, WIDTH, trees(i), 1 );
+	      trees(i) = buildRegressionTree( samplesInTree(i), i, treeImgs(i,:,:,:),  treeGazes(i,:,:), HEIGHT, WIDTH, trees(i), 1 );
 	
 	      disp(trees(i).tostring);
            end
@@ -204,7 +204,7 @@ function val = testSampleInTree(tree, node, test_img, gaze )
 
 
    if tree.isleaf(node) 
-      val = sscanf(tree.get(node),'(%f,%f)')
+      val = sscanf(tree.get(node),'(%f,%f)');
 	
    else
      
@@ -233,25 +233,33 @@ end
 
 
 
-function trees = buildRegressionTree( fatherSize, treeImgs,  treeGazes, HEIGHT, WIDTH, trees, node_i)
+function trees = buildRegressionTree( fatherSize, treeNum, treeImgs,  treeGazes, HEIGHT, WIDTH, trees, node_i)
 
 	turn = 1;
 	MAX_DEPTH = 100;
 	stackindex = 0;
 
 
-	%%% parallel staff %%%	
-	%lImgs = zeros(3,fatherSize);
-	%rImgs = zeros(3,fatherSize);
-	%final_rImgs = zeros(3, fatherSize);
-	%final_lImgs = zeros(3, fatherSize);
-	%ltree_meanGaze = zeros(3,1);
-	%rtree_meanGaze = zeros(3,1);
-	%ltreeSize = zeros(3,1);
-	%rtreeSize = zeros(3,1);
+
+		
+	ltree_tempGazes = zeros(fatherSize,2);
+	rtree_tempGazes = zeros(fatherSize,2);
+	lImgs = zeros(fatherSize);
+	rImgs = zeros(fatherSize);
+
+	final_rImgs = zeros(fatherSize);
+	final_lImgs = zeros(fatherSize);
 
 
+	minSquareError1 = zeros(3); % a huge value
+	minPx1_vert1 =     zeros(3); % something random here
+	minPx1_hor1 =     zeros(3); % also here
+	minPx2_vert1=     zeros(3); % and here..
+	minPx2_hor1 =     zeros(3); % and here 
+	bestThres1 =     zeros(3); % ah, and here
 	%%% recursion staff %%%
+
+
 	savedSize = zeros(MAX_DEPTH);
 	savedNode = zeros(MAX_DEPTH);
 	currPtrs = zeros(fatherSize);
@@ -259,231 +267,192 @@ function trees = buildRegressionTree( fatherSize, treeImgs,  treeGazes, HEIGHT, 
 	currPtrs(1) = 1;
 	for i = 2:fatherSize
 	   currPtrs(i) = currPtrs(i-1) + 1;
-	end	
-        turn = 1;
+	end
 
-labindex	
-	minSquareError = zeros(1,3);
+   turn = 1;
+   while 1 %3
+
 	
-	%minSquareError = parallel.pool.Constant(minSquareErrorX) 	
-	c = parcluster;
-	c.NumWorkers = 3;
-	saveProfile(c);
-        mypool =  parpool('local',3);%gcp('nocreate');% 
+	%for each node
+	for j =1:3
+	minSquareError1(j) = 10000; % a huge value
+	minPx1_vert1(j) =    10000; % something random here
+	minPx1_hor1(j) =     10000; % also here
+	minPx2_vert1(j)=     10000; % and here..
+	minPx2_hor1(j) =     10000; % and here 
+	bestThres1(j)  =     10000; % ah, and here
+	end
 
-	wnum = Composite(3);  % One element per worker in the pool
-     	wnum{1} = 1;
-	wnum{2} = 2;
-	wnum{3} = 3;
- spmd;
 	
-       
-       poulo = 0;
-       while poulo == 0 %3
-
-	  
-	   %for each node
-	   temp_minSquareError = distributed(zeros(1)); % a huge value
-	   temp_minSquareError = 10000;
-	   minPx1_vert =    10000; % something random here
-	   minPx1_hor =     10000; % also here
-	   minPx2_vert=     10000; % and here..
-	   minPx2_hor =     10000; % and here 
-	   bestThres  =     10000; % ah, and here
-	
-	 
-	   lImgs= zeros(1,fatherSize);
-	   rImgs = zeros(1,fatherSize);
-	   final_rImgs = zeros(1,fatherSize);
-	   final_lImgs = zeros(1,fatherSize);			           
-	   
+	for px1_vert = 1:HEIGHT
 
 
-	   for px1_vert = 1:HEIGHT		
-	         for px1_horz = 0:(WIDTH-3):3
-	            px1_hor = px1_horz + wnum;
 
-	            %for px1_hor = 1:WIDTH
-	   	    % sorry for the huge equations below
-		    % these equations are made in order to prevent 2 pixels
-		    % to be examined twice
-		    for px2_vert = ( px1_vert + floor(px1_hor/WIDTH)  ):HEIGHT
-		       for px2_hor = (1 + mod( px1_hor, WIDTH )):WIDTH
-                          if  sqrt( (px1_vert -px2_vert)^2+(px1_hor-px2_hor)^2) < 6.5             
-		             for thres = 1:25
-			        l = 0;
-			        r = 0;			
-			        meanLeftGaze = [0 0];
-			        meanRightGaze = [0 0];
-			        for j = 1:fatherSize
-			   	   if  abs(treeImgs(1, currPtrs(j), px1_vert, px1_hor) - treeImgs(1, currPtrs(j),px2_vert, px2_hor))  < thres 
-			              %left child
+	     parfor w = 1:3
+	     for px1_horz = 0:(WIDTH-3):3
+	        px1_hor = px1_horz + w
 
-			              l = l + 1;
-			              lImgs(l) = currPtrs(j); 
-			      	            
-			              meanLeftGaze(1) = meanLeftGaze(1) + treeGazes(1,currPtrs(j),1);%,:);
-			              meanLeftGaze(2) = meanLeftGaze(2) + treeGazes(1,currPtrs(j),2);%,:);	
-			           else
-			              %right child
-			              r = r + 1;
-			              rImgs(r) = currPtrs(j);  				      
+%	   for px1_hor = 1:WIDTH
+	   	% sorry for the huge equations below
+		% these equations are made in order to prevent 2 pixels
+		% to be examined twice
+		for px2_vert = ( px1_vert + floor(px1_hor/WIDTH)  ):HEIGHT
+		  for px2_hor = (1 + mod( px1_hor, WIDTH )):WIDTH
+                    if  sqrt( (px1_vert -px2_vert)^2+(px1_hor-px2_hor)^2) < 6.5             
+		     for thres = 1:50
+			
+
+
+			l = 0;
+			r = 0;			
+			meanLeftGaze = [0 0];
+			meanRightGaze = [0 0];
+			for j = 1:fatherSize
+			   	                    
+			   if  abs(treeImgs(treeNum, currPtrs(j), px1_vert, px1_hor) - treeImgs(treeNum, currPtrs(j),px2_vert, px2_hor))  < thres 
+			      %left child
+
+			      l = l + 1;
+			      ltree_tempGazes(l,w ) = treeGazes(treeNum, currPtrs(j) );
+			      lImgs(w,l) = currPtrs(j); 
+			       				      
 			      
-			              meanRightGaze(1) = meanRightGaze(1) + treeGazes(1,currPtrs(j),1);%,:);
-			              meanRightGaze(2) = meanRightGaze(2) + treeGazes(1,currPtrs(j),2);
-			           end
-			        end
+			      meanLeftGaze(1) = meanLeftGaze(1) + treeGazes(treeNum,currPtrs(j),1);%,:);
+			      meanLeftGaze(2) = meanLeftGaze(2) + treeGazes(treeNum,currPtrs(j),2);%,:);	
+			  
 	
-			        meanLeftGaze = meanLeftGaze  / l;
-			        meanRightGaze = meanRightGaze/ r;
+			   else
+			      %right child
 
-			        squareError = 0;
-			        for j = 1:r
-	 		           squareError=squareError + (meanRightGaze(1)-treeGazes(1,rImgs(r), 1))^2 + (meanRightGaze(2)-treeGazes(1,rImgs(r), 2))^2;
-		                end
-			        for j = 1:l	
-  			           squareError=squareError + (meanLeftGaze(1)-treeGazes(1,lImgs(l), 1))^2 + (meanRightGaze(2)-treeGazes(1,lImgs(l), 2))^2;	
-			        end
+			      r = r + 1;
+			      rtree_tempGazes(r) = treeGazes(treeNum,currPtrs(j) );
+			      rImgs(r) = currPtrs(j);  				      
+			      
+			      meanRightGaze(1) = meanRightGaze(1) + treeGazes(treeNum,currPtrs(j),1);%,:);
+			      meanRightGaze(2) = meanRightGaze(2) + treeGazes(treeNum,currPtrs(j),2);
+			   end
+			end
+	
+			meanLeftGaze = meanLeftGaze  / l;
+			meanRightGaze = meanRightGaze/ r;
+
+			squareError = 0;
+			for j = 1:r
+			   squareError=squareError + (meanRightGaze(1)-rtree_tempGazes(j,1))^2 + (meanRightGaze(2)-rtree_tempGazes(j,2))^2;	
+			end
+			for j = 1:l
+			   squareError=squareError + (meanLeftGaze(1)-ltree_tempGazes(j,1) )^2 + (meanLeftGaze(2)-ltree_tempGazes(j,2))^2;	
+			end
 		
-			        if squareError < temp_minSquareError
-			           minSquareError(wnum) = squareError;	
-		
-			           minPx1_vert =    px1_vert; % something random here
-			           minPx1_hor =     px1_hor; % also here
-			   	   minPx2_vert=     px2_vert; % and here..
-			   	   Px2_hor =     px2_hor; % and here
-			   	   bestThres  =     thres;
+			if squareError < minSquareError
+			   minSquareError1(w) = squareError;			
+			   minPx1_vert1(w) =    px1_vert; % something random here
+			   minPx1_hor1(w)  =     px1_hor; % also here
+			   minPx2_vert1(w)=     px2_vert; % and here..
+			   minPx2_hor1(w) =     px2_hor; % and here
+			   bestThres1(w)  =     thres;
 			   
 			   
-			   	   for o = 1:r
-			              final_rImgs(o) = rImgs(o);%%%%%%%%%%%%
-			           end
+			   for o = 1:r
+			      final_rImgs(w,o) = rImgs(o);%%%%%%%%%%%%
+			   end
 
-			   	   for o = 1:l
-			              final_lImgs(o) = lImgs(o);%%%%%%%%%%%%
-			           end				
+			   for o = 1:l
+			      final_lImgs(w,o) = lImgs(o);%%%%%%%%%%%%
+			   end				
 
-			   	   ltreeSize = l;
-			   	   rtreeSize = r;
+			   ltree.size = l;
+			   rtree.size = r;
+			   if node_i == 2
+			       ltree.size 
+			   rtree.size 
+			   end		
+                           rtree.meanGaze = meanRightGaze;
+			   ltree.meanGaze = meanLeftGaze;
+			end
+			 		 	
+		     end%thres
+		    end%end if < 6.5
 		
-                           	   rtree_meanGaze = meanRightGaze;
-			   	   ltree_meanGaze = meanLeftGaze;
-				end	 	
-		             end%thres
-		          end%end if < 6.5	
-		       end%px2_hor
-		    end%px2_vers 	
-	         end %px1_hor
-           end %endof px1_vert
+		   end%px2_hor
+		end%px2_vers
+ 	      end
+	   end	
+	  	
+        end
+	
+	minError = minSquareError(1); 
+	min_i = 1; 
+	for j = 2:3
+	  if minSquareError(j) < minError
+	     minError = minSquareError(j);
+	     min_i = j;	
+	  end 
+	end
 
 
-	   %%% store the workers results %%%
-	   %lImgs(w,:) = temp_lImgs;
-	   %rImgs(w,:) = temp_rImgs;
-	   %final_rImgs(w,:) = temp_final_rImgs(o);
-	   %final_lImgs(w,:) = temp_final_Imgs(o);
-	   %ltreeSize(w) = temp_ltreeSize;
-	   %rtreeSize(w) = temp_rtreeSize;
-	   %rtree_meanGaze(w) =  temp_rtree_meanGaze;
-	   %ltree_meanGaze(w) =  temp_ltree_meanGaze;
- 
-
-	   labBarrier;
-	 	
-          %dataReceived = labSendReceive(rcvWkrIdx,srcWkrIdx,dataSent)
-
-	 
-	   
-	   %receive from right
-	   minSquareError(1+mod(wnum,3)) = labSendReceive(1+ mod(wnum-2,3) , 1+mod(wnum,3)    ,minSquareError(wnum) );
-
-	   labBarrier;
-	   %receive from left
-	   minSquareError(1+ mod(wnum-2,3)) = labSendReceive( 1+mod(wnum,3), 1+ mod(wnum-2,3) ,minSquareError(wnum) );
-	   
-labBarrier;
-	   %minSquareError(wnum) =  minSquareError(wnum) + temp_minSquareError
+	minSquareError =  minSquareError1(j); 
+	minPx1_vert = minPx1_vert1(j);  
+	minPx1_hor = minPx1_hor1(j);     
+	minPx2_vert = minPx2_vert1(j);     
+	minPx2_hor  =minPx2_hor1(j);      
+	bestThres = bestThres1(j);      
 	
 
-
-
-	   %%% sychronize before finding the best worker %%%
-	   labBarrier;
-	 	
-
-	   bestWorker = 1;
-	   minError = minSquareError(1);	
-	   for k = 2:3
-	      if minSquareError(k) < minError
-	         minError = minSquareError(k);
- 		 bestWorker = k;
-	      end
-	   end
-	
-
-        if bestWorker == wnum
-
-	   %%%%%% Recursion starts here %%%%%	
-	   if (ltreeSize > 0 && rtreeSize > 0)
-  	      turn = 1;
+	%%%%%% Recursion starts here %%%%%	
+	if (ltree.size > 0 && rtree.size > 0)
+  	   turn = 1;
  
-              trees=trees.set(node_i,strcat('Samples:',num2str(fatherSize),',px1(', num2str(minPx1_vert),',',num2str(minPx1_hor),')-','px2(',num2str(minPx2_vert),',',num2str(minPx2_hor),')>=', num2str(bestThres) ));  
+           trees=trees.set(node_i,strcat('Samples:',num2str(fatherSize),',px1(', num2str(minPx1_vert),',',num2str(minPx1_hor),')-','px2(',num2str(minPx2_vert),',',num2str(minPx2_hor),')>=', num2str(bestThres) ));  
 
-	      [trees lnode] = trees.addnode(node_i, strcat('(', num2str(ltree_meanGaze(1)), ',', num2str(ltree_meanGaze(2)), ')'));
-	      [trees rnode] = trees.addnode(node_i, strcat('(', num2str(rtree_meanGaze (1)), ',', num2str(rtree_meanGaze (2)), ')'));
+
+
+	   [trees lnode] = trees.addnode(node_i, strcat('(', num2str(ltree.meanGaze(1)), ',', num2str(ltree.meanGaze(2)), ')'));
+	   [trees rnode] = trees.addnode(node_i, strcat('(', num2str(rtree.meanGaze (1)), ',', num2str(rtree.meanGaze (2)), ')'));
 
 	      % start saving the left brother
 	      
 	      stackindex = stackindex + 1;
-	      %fprintf('push:\n');
-	      savedSize(stackindex) = ltreeSize;
+	      fprintf('push:\n');
+	      savedSize(stackindex) = ltree.size;
 	      %savedSize(stackindex)
 	      savedNode(stackindex) = lnode;
-		
-	      for o = 1:ltreeSize
+		lnode
+	      for o = 1:ltree.size
 	         savePtrs(stackindex,o) = final_lImgs(o);
 	      end
 
 	      %%%   prepare data for right son %%%
 	      node_i = rnode;
-	      fatherSize = rtreeSize;
-	      for o = 1:rtreeSize
+	      fatherSize = rtree.size;
+	      for o = 1:rtree.size
 		 currPtrs(o) = final_rImgs(o);
 	      end	
- 
-           else  %2
-	      if stackindex == 0 
-		 delete( mypool );
 
-		 poulo = 1;		         
-	      else 
-	         %%%   prepare next iteration data %%%  
-	         %fprintf('pop:\n'); 
-	         fatherSize = savedSize(stackindex);
-	         node_i = savedNode(stackindex);
- 	         %node_i
-	         for o = 1:fatherSize
-	            currPtrs(o) = savePtrs(stackindex,o);
-	         end
-	         stackindex = stackindex - 1;	
+	else %2
+	   if stackindex == 0 
+	         break;
+	   end 
+	   %%%   prepare next iteration data %%%  
+	   fprintf('pop:\n'); 
+	   fatherSize = savedSize(stackindex);
+	   node_i = savedNode(stackindex);
+ 		node_i
+	   for o = 1:fatherSize
+	      currPtrs(o) = savePtrs(stackindex,o);
+	   end
+	   stackindex = stackindex - 1;	
 
-	         if turn 
-	            turn = 0;
-	         else%1 
-	            %ftiakse node2
-	         end%1
-	      end
-	   end %2	
-	   %stackindex
-	   %turn
-         
+	   if turn 
+	      turn = 0;
+	   else%1 
+	      
+	   end%1
+	end %2	
+	
+	%stackindex
+	%turn
+   end %3
 
-        end %if bestWorker == labindex 
 
-	%%% sychronize all threads before the next loop %%%	
-	labBarrier;
-   end %while loop
-
-   end
 end
-
