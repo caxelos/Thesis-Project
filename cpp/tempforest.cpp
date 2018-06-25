97911aeaf68a82bdee61d3f94c58a64b2c4f4e75
trakaros <caxelos@inf.uth.gr>
2018-06-26 02:39:46 +0300

    cpp 1%

--- cpp/tempforest.cpp
+++ cpp/tempforest.cpp
@@ -16,19 +16,15 @@ const int NY = 13;
 
 int main(void)  {
    int curr_nearest[13];
+   float center[2];
+
    int rank;
-   hsize_t      offset[2];	// hyperslab offset in the file
-   hsize_t      count[2];	// size of the hyperslab in the file111
-   hsize_t     dimsm[2]; /* memory space dimensions */
-   hsize_t offset_out[2];
-   hsize_t count_out[2];
-   hsize_t dims_out[2];
+   hsize_t     dims[4]; /* memory space dimensions */
 
    H5File *file = NULL;
    Group *group = NULL;
 
-   DataSet dataset;
-   DataSpace dataspace;
+  
    /*
     * Try block to detect exceptions raised
     */   
@@ -42,56 +38,54 @@ int main(void)  {
       
       file = new H5File(FILE_NAME, H5F_ACC_RDWR);
       group = new Group(file->openGroup("g1") );   
- 
-/*
- *****************************************************
-      12_nearestIDS
-*******************************************************
-*/
-      dataset = group->openDataSet("12_nearestIDs");     
-      dataspace = dataset.getSpace();//dataspace???
-      rank = dataspace.getSimpleExtentDims( dimsm );// get rank = numOfDims
-      //dataset      
-      offset[0] = 0;
-      offset[1] = 0;
-      count[0]  = dimsm[0];
-      count[1]  = dimsm[1];
-      dataspace.selectHyperslab( H5S_SELECT_SET, count, offset );
 
-      //memory hyperslab
-       DataSpace memspace( rank, dimsm );     
-      offset_out[0] = 0;
-      offset_out[1] = 0;
-      count_out[0] = dimsm[0]; 
-      count_out[1] = dimsm[1]; 
-      memspace.selectHyperslab( H5S_SELECT_SET, count_out, offset_out );
  
+     /*
+      * 12_nearestIDS
+      */
+      DataSet dataset = group->openDataSet("12_nearestIDs");     
+      DataSpace dataspace = dataset.getSpace();//dataspace???
+      rank = dataspace.getSimpleExtentDims( dims );// get rank = numOfDims
+      DataSpace memspace( rank, dims );     
       dataset.read(curr_nearest, PredType::NATIVE_INT, memspace, dataspace ); 
-/*
- *****************************************************
-      center:1x2
-*******************************************************
-*/
+    
+
+
+     /*
+      * center
+      */
       dataset = group->openDataSet("center");     
       dataspace = dataset.getSpace();//dataspace???
-      rank = dataspace.getSimpleExtentDims( dimsm );// get rank = numOfDims
+      rank = dataspace.getSimpleExtentDims( dims );// get rank = numOfDims
+      memspace.setExtentSimple( rank, dims );
+      dataset.read(center, PredType::NATIVE_FLOAT, memspace, dataspace ); 
+   
 
-      //dataset      
-      offset[0] = 0;
-      offset[1] = 0;
-      count[0]  = dimsm[0];
-      count[1]  = dimsm[1];
-      dataspace.selectHyperslab( H5S_SELECT_SET, count, offset );
-
-      //memory database	
-      dimsm[0] = dimsm[0];
-      dimsm[1] = dimsm[1];
-      DataSpace memspace( rank, dimsm );
-
+     /*
+      * gaze
+      */
+      dataset = group->openDataSet("gaze");     
+      dataspace = dataset.getSpace();//dataspace???
+      rank = dataspace.getSimpleExtentDims( dims );// get rank = numOfDims
+      memspace.setExtentSimple( rank, dims );
 
 
-      cout << "dims are: " << dimsm[0] << ", " << dimsm[1];
+     /*
+      * headpose
+      */
+      dataset = group->openDataSet("headpose");     
+      dataspace = dataset.getSpace();//dataspace???
+      rank = dataspace.getSimpleExtentDims( dims );// get rank = numOfDims
+      memspace.setExtentSimple( rank, dims );
 
+
+      /*
+      * data
+      */
+      dataset = group->openDataSet("data");     
+      dataspace = dataset.getSpace();//dataspace???
+      rank = dataspace.getSimpleExtentDims( dims );// get rank = numOfDims
+      memspace.setExtentSimple( rank, dims );//24x1x9x15
 
 
 
@@ -116,3 +110,42 @@ int main(void)  {
 
    return 0;
 }
+
+/*
+#include<iostream>
+#include<vector>
+#include<H5Cpp.h>
+
+
+const std::string& filename = "mydata.h5";
+
+std::vector<double> load( const std::string& dataset) {
+  
+  H5::DataSet dset = fp.openDataSet(dataset.c_str());
+  H5::DataSpace dspace = dset.getSpace();
+  hsize_t rank;
+  hsize_t dims[2];  
+  rank = dspace.getSimpleExtentDims(dims, nullptr);
+  std::vector<double> data;
+  data.resize(dims[0]);
+  dset.read(data.data(), H5::PredType::NATIVE_DOUBLE, dspace);
+  
+  return data;
+}
+
+int main() {
+
+  H5::H5File fp(filename.c_str(), H5F_ACC_RDONLY);
+  std::vector<double> data = {1, 2};
+
+  auto data_read = load(fp, "my dataset");
+  for(auto item: data_read) {
+    std::cout<<item<<" ";
+  }
+  std::cout<<std::endl;
+  fp.close();
+
+
+  return 0;
+}
+*/
