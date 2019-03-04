b71acaa6f691614302eb58ef20a99ba7680e9dc2
Christos Axelos <caxelos@inf.uth.gr>
2019-03-04 03:08:14 +0200

    face-landmark-pose estimation

--- dlib/webcam_face_pose_ex.cpp
+++ dlib/webcam_face_pose_ex.cpp
@@ -27,16 +27,13 @@
     2011.  SSE4 is the next fastest and is supported by most current machines.  
 */
 
-//#include "../Downloads/dlib-19.16/dlib/opencv.h"
+
 #include <dlib/opencv.h>
 #include <opencv2/highgui/highgui.hpp>
-//#include "../Downloads/dlib-19.16/dlib/image_processing/frontal_face_detector.h"
+#include <opencv2/calib3d/calib3d.hpp>
 #include <dlib/image_processing/frontal_face_detector.h>
-//#include "../Downloads/dlib-19.16/dlib/image_processing/render_face_detections.h"
 #include <dlib/image_processing/render_face_detections.h>
-//#include "../Downloads/dlib-19.16/dlib/image_processing.h"
 #include <dlib/image_processing.h>
-//#include "../Downloads/dlib-19.16/dlib/gui_widgets.h"
 #include <dlib/gui_widgets.h>
 
 using namespace dlib;
@@ -58,17 +55,29 @@ int main()
         // Load face detection and pose estimation models.
         frontal_face_detector detector = get_frontal_face_detector();
         shape_predictor pose_model;
-        deserialize("shape_predictor_68_face_landmarks.dat") >> pose_model;
+        deserialize("../../Downloads/shape_predictor_68_face_landmarks.dat") >> pose_model;
+	
+		//define our 3D face model:
+		std::vector<cv::Point3d> model3Dpoints;
+    	model3Dpoints.push_back(cv::Point3d(-45.097f, -0.48377f, 2.397f));// Right eye: Right Corner
+   		model3Dpoints.push_back(cv::Point3d(-21.313f, 0.48377f, -2.397f));// Right eye: Left Corner
+    	model3Dpoints.push_back(cv::Point3d(21.313f, 0.48377f, -2.397f));// Left eye: Right Corner
+    	model3Dpoints.push_back(cv::Point3d(45.097f, -0.48377f, 2.397f));// Left eye: Left Corner
+    	model3Dpoints.push_back(cv::Point3d(-26.3f, 68.595f, -9.8608e-32f));// Mouth: Right Corner
+    	model3Dpoints.push_back(cv::Point3d(26.3f, 68.595f, -9.8608e-32f));//Mouth: Left Corner	 
 
+    	
         // Grab and process frames until the main window is closed by the user.
         while(!win.is_closed())
         {
+		
             // Grab a frame
             cv::Mat temp;
             if (!cap.read(temp))
             {
                 break;
             }
+            //printf("rows are %d\n", temp.rows);//cols=640,rows=460
             // Turn OpenCV's Mat into something dlib can deal with.  Note that this just
             // wraps the Mat object, it doesn't copy anything.  So cimg is only valid as
             // long as temp is valid.  Also don't do anything to temp that would cause it
@@ -80,14 +89,50 @@ int main()
             // Detect faces 
             std::vector<rectangle> faces = detector(cimg);
             // Find the pose of each face.
-            std::vector<full_object_detection> shapes;
-            for (unsigned long i = 0; i < faces.size(); ++i)
-                shapes.push_back(pose_model(cimg, faces[i]));
+            std::vector<full_object_detection> shapes2d;
+            for (unsigned long i = 0; i < faces.size(); ++i) {
+                full_object_detection shape = pose_model(cimg,faces[i]);//to pose model den einai function!
+                std::vector<cv::Point2d> image_points;
 
-            // Display it all on the screen
+                image_points.push_back(cv::Point2d(shape.part(36).x(),shape.part(36).y()));    // Nose tip
+                image_points.push_back(cv::Point2d(shape.part(39).x(),shape.part(39).y()));    // Chin
+                image_points.push_back(cv::Point2d(shape.part(42).x(),shape.part(42).y()));    // Left eye left corner
+                image_points.push_back(cv::Point2d(shape.part(45).x(),shape.part(45).y()));    // Right eye right corner
+                image_points.push_back(cv::Point2d(shape.part(48).x(),shape.part(48).y()));    // Left Mouth corner
+                image_points.push_back(cv::Point2d(shape.part(54).x(),shape.part(54).y()));    // Right mouth corner
+                //tha xreiastoume ta 6 parakatw 2d landmarks(se pixels):
+                //n.36: deksia akri deksiou matiou
+                //n.39: aristeri akri deksiou matiou
+                //n.42: deksia akri aristerou matiou
+                //n.45: aristeri akri aristerou matiou
+                //n.48: deksia akri stoma
+                //n.54: aristeri akri stoma                                
+                cout << "pixel position of 1st part: " << shape.part(0) << endl;
+                shapes2d.push_back(pose_model(cimg, faces[i]));//size=68, shape.part(0)         
+
+
+				//Camera internals. rows=460, cols=640
+    			double focal_length = temp.cols;// Approximate focal length.
+   				cv::Point2d center = cv::Point2d(temp.cols/2,temp.rows/2);
+    			cv::Mat camera_matrix = (cv::Mat_<double>(3,3) << focal_length, 0, center.x, 0 , focal_length, center.y, 0, 0, 1);
+    			cv::Mat dist_coeffs = cv::Mat::zeros(4,1,cv::DataType<double>::type);// Assuming no lens distortion
+				cout << "Camera Matrix " << endl << camera_matrix << endl ;
+    	
+    			// Output rotation and translation
+    			cv::Mat rotation_vector; // Rotation in axis-angle form
+    			cv::Mat translation_vector;	
+				// Solve for pose
+    			cv::solvePnP(model3Dpoints, image_points, camera_matrix, dist_coeffs, rotation_vector, translation_vector); 
+        	}
+            
+
+            //Now let's view our face poses on the screen.
+            /*
             win.clear_overlay();
             win.set_image(cimg);
-            win.add_overlay(render_face_detections(shapes));
+            win.add_overlay(render_face_detections(shapes2d)); 
+	    	*/
+	    
         }
     }
     catch(serialization_error& e)
@@ -103,4 +148,19 @@ int main()
     }
 }
 
+	
 
+/*
+    	vector<Point3d> nose_end_point3D;//Project a 3D point (0,0,1000.0) onto the image plane.
+    	vector<Point2d> nose_end_point2D;
+    	nose_end_point3D.push_back(Point3d(0,0,1000.0));
+     
+    	projectPoints(nose_end_point3D, rotation_vector, translation_vector, camera_matrix, dist_coeffs, nose_end_point2D);
+*/
+
+
+
+//dlib::array<array2d<rgb_pixel> > face_chips;
+//extract_image_chips(cimg, get_face_chip_details(shapes), face_chips);
+//win.set_image(tile_images(face_chips));	
+        
