a0a6570ce78a31902054a2b955558a4ea43429e6
Christos Axelos <caxelos@inf.uth.gr>
2020-01-08 00:54:50 +0200

    add resnet multiview/mpiigaze data

--- python/pytorch/dataloader.py
+++ python/pytorch/dataloader.py
@@ -33,8 +33,10 @@ class MPIIGazeDataset(torch.utils.data.Dataset):
 def get_loader(dataset_dir, test_subject_id, batch_size, num_workers, use_gpu):
     assert os.path.exists(dataset_dir)
     assert test_subject_id in range(15)
+
     subject_ids = ['p{:02}'.format(index) for index in range(15)]
     test_subject_id = subject_ids[test_subject_id]
+    print("test:",test_subject_id)
 
     train_dataset = torch.utils.data.ConcatDataset([
         MPIIGazeDataset(subject_id, dataset_dir) for subject_id in subject_ids
@@ -44,6 +46,50 @@ def get_loader(dataset_dir, test_subject_id, batch_size, num_workers, use_gpu):
 
     assert len(train_dataset) == 42000
     assert len(test_dataset) == 3000
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=use_gpu,
+        drop_last=True,
+    )
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        shuffle=False,
+        pin_memory=use_gpu,
+        drop_last=False,
+    )
+    return train_loader, test_loader
+
+
+# train_loader, test_loader = get_loader(
+#        args.dataset=data, args.test_id=0, args.batch_size=32, args.num_workers, True)
+
+def get_loader_multiview_mpiigaze(trainset_dir, testset_dir, batch_size, num_workers, use_gpu):
+#def get_loader_multiview_mpiigaze(dataset_dir, test_subject_id, batch_size, num_workers, use_gpu):
+    
+
+    assert os.path.exists(trainset_dir)
+    assert os.path.exists(testset_dir)
+    #assert test_subject_id in range(15)
+
+    trainsubject_ids = ['p{:02}'.format(index) for index in range(50)]
+    testsubject_id = ['p{:02}'.format(index) for index in range(15)]
+
+    train_dataset = torch.utils.data.ConcatDataset([
+        MPIIGazeDataset(subject_id, trainset_dir) for subject_id in trainsubject_ids
+    ])
+    train_dataset = torch.utils.data.ConcatDataset([
+        MPIIGazeDataset(subject_id, testset_dir) for subject_id in trainsubject_ids
+    ])
+
+
+    assert len(train_dataset) == 64000
+    assert len(test_dataset) == 45000
 
     train_loader = torch.utils.data.DataLoader(
         train_dataset,
