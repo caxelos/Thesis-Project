a4257f0deccb183ee69e52e45c4601406394883d
Christos Axelos <caxelos@inf.uth.gr>
2020-01-07 20:35:00 +0200

    final multiview preprocess

--- python/pytorch/preprocess_multiview.py
+++ python/pytorch/preprocess_multiview.py
@@ -30,28 +30,29 @@ def main():
 	folders = ['s00-09','s10-19','s20-29','s30-39','s40-49']
 	
 
-	totals={}
-	totals['sum']=0
-	#calc how many from each file
-	for folder in folders:
-		subfolder = os.listdir(root_dir+folder)
-		for sij in subfolder:
-			totals[sij] = {}
+	# totals={}
+	# totals['sum']=0
+	# #calc how many from each file
+	# for folder in folders:
+	# 	subfolder = os.listdir(root_dir+folder)
+	# 	for sij in subfolder:
+	# 		totals[sij] = {}
 
-			curr_path=root_dir+folder+'/'+sij+'/synth/'
-			dirlist=os.listdir(curr_path)
-			for name in dirlist:
-				if os.path.isdir(os.path.join(curr_path,name)):
-					data =pd.read_csv(curr_path+name+'.csv',header=None,usecols=[0,1,2,3,4,5]).values#read only gaze and pose
-					totals[sij][name]=int(round(64000*len(data)/230400))
-					totals['sum'] = totals['sum']+totals[sij][name]
-					
-			print(sij)
-	json.dump(totals, open("save_UT_Multiview.txt",'w'))
+	# 		curr_path=root_dir+folder+'/'+sij+'/synth/'
+	# 		dirlist=os.listdir(curr_path)
+	# 		for name in dirlist:
+	# 			if os.path.isdir(os.path.join(curr_path,name)):
+	# 				data =pd.read_csv(curr_path+name+'.csv',header=None,usecols=[0,1,2,3,4,5]).values#read only gaze and pose
+	# 				totals[sij][name]=int(round(64000*len(data)/230400))
+	# 				totals['sum'] = totals['sum']+totals[sij][name]
+
+	# 		print(sij)
+	# json.dump(totals, open("save_UT_Multiview.txt",'w'))
 	
-	error("poulo")
-	totals=json.load(open("save_UT_Multiview.txt"))
-
+	#error("ok")
+	#totals=json.load(open("save_UT_Multiview.txt"))
+	num_img=0
+	num_data=0
 	for folder in folders:
 		subfolder = os.listdir (root_dir+folder)
 		for sij in subfolder:
@@ -65,13 +66,17 @@ def main():
 					data =pd.read_csv(curr_path+name+'.csv',header=None,usecols=[0,1,2,3,4,5]).values#read only gaze and pose
 					img_list=os.listdir(curr_path+name)
 					img_list.sort()#print(type(df.values))
-					indices=np.random.choice(data.shape[0],totals[sij][name],replace=False)
+					indices=np.random.choice(144,5,replace=False)#5 out of 144 eye imgs
+					for i in indices:#144 images
+						img = img_list[i]
+						num_img=num_img+1
 
-					for img in img_list:#144 images
 						image = cv2.imread(curr_path+name+"/"+img, 0)
 						images.append(image)
-					samples=samples+len(data)
-					for i in range(len(data)):
+					print(len(images))
+					
+					for i in indices:#range(len(data)):
+						num_data=num_data+1
 						if 'r' in name: #right eye
 							gaze=convert_gaze(data[i,0:3])*np.array([-1,1])
 							pose=convert_pose(data[i,3:6])*np.array([-1,1])
@@ -82,7 +87,7 @@ def main():
 							
 						poses.append(pose)
 						gazes.append(gaze)
-						
+						print(len(gazes))	
 								
 						#pose = convert_pose(right_poses[day][index]) * np.array([-1, 1])
 						#gaze = convert_gaze(right_gazes[day][index]) * np.array([-1, 1])
@@ -98,8 +103,11 @@ def main():
 
 			outpath = os.path.join("data_UT_Multiview", sij)
 			np.savez(outpath, image=images, pose=poses, gaze=gazes)
+			samples=samples+len(gazes)#len(images)
 			print("till now:", samples, " samples!")
 	print("total samples:",samples)
+	print("images:",num_img)
+	print("data:",num_data)
 
 				
 
